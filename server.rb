616d4a89f08bd44a5a65458b61feb7c08fd9156f
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-14 18:01:07 +0000

    Fixes user sign out(adds delete sessions)

--- server.rb
+++ server.rb
@@ -74,6 +74,12 @@ class BookmarkManager < Sinatra::Base
     end
   end
 
+  delete '/sessions' do
+    flash[:notice] = "Goodbye!"
+    session[:user_id] = nil
+    redirect to('/')
+  end
+
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id]) if session[:user_id]
--- spec/user_sign_in_spec.rb
+++ spec/user_sign_in_spec.rb
@@ -22,6 +22,20 @@ feature "User signs in" do
 		expect(page).not_to have_content("Welcome test@test.com")
 	end
 
+	before(:each) do
+      User.create(email: "test@test.com",
+      password: "test",
+      password_confirmation: "test")
+  end
+
+  scenario "while being signed in" do
+    sign_in("test@test.com","test")
+    click_button('Sign out')
+    expect(page).to have_content("Goodbye!")
+    expect(page).not_to have_content("Welcome test@test.com")
+  end
+
+
 	def sign_in(email, password)
 		visit '/sessions/new'
 		fill_in 'email', with: email
