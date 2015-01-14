d7ab841e3a7c167e7527e02d435b823fa76ca12e
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-14 13:01:22 +0000

    Adds flash message if pswd does not match confirmation pswd

--- server.rb
+++ server.rb
@@ -1,10 +1,11 @@
 require 'sinatra/base'
 require 'data_mapper'
-
+require 'rack-flash'
 
 class BookmarkManager < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
+  use Rack::Flash
 
 	env = ENV['RACK_ENV'] || 'development'
 	DataMapper.setup(:default, "postgres://localhost:5432/bookmark_manager_#{env}")
@@ -45,15 +46,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email], 
+    @user = User.create(email: params[:email], 
       password: params[:password],
       password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect '/'
+    if @user.save 
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      flash[:notice]="Sorry, your passwords don't match."
+      erb :'/users/new'
+    end
   end
 
   helpers do
--- spec/user_management_spec.rb
+++ spec/user_management_spec.rb
@@ -1,5 +1,6 @@
 require 'user'
 
+
 feature 'User signs up' do
 
   scenario 'when being a new user visiting the site' do
@@ -10,6 +11,8 @@ feature 'User signs up' do
 
   scenario "with a password that doesn't match" do
     expect{ sign_up('a@a.com', 'pass', 'wrong')}.to change(User, :count).by 0
+    expect(current_path).to eq '/users'
+    expect(page).to have_content("Sorry, your passwords don't match.")
   end
 
   def sign_up(email="steph@gmail.com",
