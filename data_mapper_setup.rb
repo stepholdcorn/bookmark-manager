6d6ea425b3e665eb1e7e7e35768ba90621306d4b
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-15 11:25:05 +0000

    Adds to Heroku

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -1,5 +1,7 @@
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost:5432/bookmark_manager_#{env}")
+url = ENV['DATABASE_URL'] || "postgres://localhost:5432/bookmark_manager_#{env}"
+
+DataMapper.setup(:default, url)
 DataMapper.finalize
